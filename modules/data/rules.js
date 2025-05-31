1c9f71f07f1eca93a96ddcb980fbf3653fdd0564
maia1600 <MAIA1600@GMAIL.COM>
2025-05-31 10:35:25 +0100

    Update rules.js

--- modules/data/rules.js
+++ modules/data/rules.js
@@ -1,1 +1,28 @@
-
+export const rules = [
+  {
+    categoria: "upsell",
+    exemplo: "💡 Oferecemos carro de substituição gratuito para orçamentos acima de 300€."
+  },
+  {
+    categoria: "urgencia",
+    regras: [
+      {
+        condicao: "cliente menciona pressa, urgência ou prazo apertado",
+        acao: "responder com prioridade e sugerir marcação imediata"
+      }
+    ]
+  },
+  {
+    categoria: "objeções",
+    regras: [
+      {
+        condicao: "cliente menciona preço alto",
+        acao: "responder com empatia e reforçar o valor e durabilidade do serviço"
+      },
+      {
+        condicao: "cliente menciona falta de tempo",
+        acao: "sugerir carro de substituição ou recolha gratuita"
+      }
+    ]
+  }
+];
