c16017ba169df6681f8cc1a86434cc748c581ed6
maia1600 <MAIA1600@GMAIL.COM>
2025-06-01 09:46:14 +0100

    Update EditarRegraButton.js

--- modules/components/Regras/EditarRegraButton.js
+++ modules/components/Regras/EditarRegraButton.js
@@ -1,1 +1,13 @@
 
+import React from 'react';
+
+export default function EditarRegraButton({ onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-1 px-3 rounded"
+    >
+      Editar
+    </button>
+  );
+}
