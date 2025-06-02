03bdfceeaacf1afae587a49dad3238ae188b2a37
maia1600 <MAIA1600@GMAIL.COM>
2025-06-02 14:57:24 +0100

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -50,7 +50,7 @@ const extrairServicosDaMensagem = (mensagem, conhecimentoBase) => {
 
   return encontrados;
 }
-};
+
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', 'https://tamai.pt');
