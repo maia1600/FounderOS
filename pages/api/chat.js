61ccdf77a7ac2828fffd25c0d87583917adb6fee
maia1600 <MAIA1600@GMAIL.COM>
2025-06-16 09:44:48 +0100

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-         Authorization: `api_key ${process.env.RELEVANCE_API_KEY}`,
+         Authorization: `project:${process.env.RELEVANCE_API_KEY}`
 
         },
         body: JSON.stringify({
